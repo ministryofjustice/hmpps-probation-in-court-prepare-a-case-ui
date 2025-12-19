3828f7c79f2a43e0f2a0228611289068d377b3a4
Aron-v1 <aron.marriottsmith@justice.gov.uk>
2025-12-19 10:25:27 +0000

    Fix logger code formating for linter

--- logger.ts
+++ logger.ts
@@ -4,6 +4,10 @@ import config from './server/config'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
 
-const logger = bunyan.createLogger({ name: 'HMPPS Probation In Court Prepare A Case Ui', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({
+  name: 'HMPPS Probation In Court Prepare A Case Ui',
+  stream: formatOut,
+  level: 'debug',
+})
 
 export default logger
